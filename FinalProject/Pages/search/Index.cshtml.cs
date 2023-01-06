9974160768a7d1b14e3b9024398b0ecce96b1ab1
Rachel ✨ <rachelajayi08@gmail.com>
2023-01-06 18:32:32 +0000

    updated razor pages from google drive

---
+++ FinalProject/Pages/search/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorPageFinalProject.Pages.search
+{
+
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+
+    }
+}
