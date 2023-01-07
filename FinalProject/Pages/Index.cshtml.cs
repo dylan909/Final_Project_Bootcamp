04386e7c7909a6aa526dd72221e7fb37ad97e9c7
dylan909 <73878448+dylan909@users.noreply.github.com>
2023-01-07 20:11:48 +0000

    major update

--- FinalProject/Pages/Index.cshtml.cs
+++ FinalProject/Pages/Index.cshtml.cs
@@ -11,17 +11,6 @@ namespace FinalProject.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly FinalProjectContext _context;
-
-    public IndexModel(FinalProjectContext context) => _context = context;
-
-    public List<Teams>? Teams { get; set; }
-
-
-    public async Task OnGetAsync()
-    {
-        Teams = await _context.teams.ToListAsync();
-    }
 }
 
 
---
+++ FinalProject/Pages/Teams/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Data;
+using FinalProject.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Pages.Teams
+{
+	public class IndexModel : PageModel
+    {
+        private readonly FinalProjectContext _context;
+
+        public IndexModel(FinalProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<FinalProject.Model.Teams> Teams { get; set; }
+        public async Task OnGetAsync()
+        {
+            Teams = await _context.teams.ToListAsync();
+        }
+
+
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            FinalProject.Model.Players? teamDeleted = await _context.teams.FindAsync(id);
+
+            if (teamDeleted != null)
+            {
+                _context.allplayers.Remove(teamDeleted);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
