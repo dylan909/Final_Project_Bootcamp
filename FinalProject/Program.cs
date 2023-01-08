f18eda084decbab53fced1b0179f0c6d74aaf9ff
bilalakuji <113610858+bilalakuji@users.noreply.github.com>
2023-01-08 16:15:38 +0000

    Tweaks in the "all players" tab and added some middleware functions

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -2,15 +2,21 @@
 using Microsoft.Extensions.Configuration;
 using FinalProject.Data;
 using System.Configuration;
+using Microsoft.AspNetCore.Authorization;
 using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//Checks app & database status 
+builder.Services.AddHealthChecks();
 
+builder.Services.AddResponseCaching();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<FinalProjectContext>(option => option.UseSqlite(builder.Configuration.GetConnectionString("FinalProjectContext")));
 
@@ -24,16 +30,34 @@ void CreateDbIfNotExists(IHost host)
     }
 }
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// Status code if page cannot be found
+app.UseStatusCodePages(async statusCodeContext =>
+{
+    statusCodeContext.HttpContext.Response.ContentType = Text.Plain;
+
+    await statusCodeContext.HttpContext.Response.WriteAsync(
+        $"Status Code Page: {statusCodeContext.HttpContext.Response.StatusCode}");
+});
+
+app.UseHttpsRedirection();
+
+//configures database healthcheck
+app.MapHealthChecks("/healthz")
+    .RequireAuthorization();
+
+
+
+app.UseResponseCaching();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
